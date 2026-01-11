cbb2a5d678f598953232bbb327321a88520717b1
abually <denny.setiawan002@gmail.com>
2026-01-11 16:43:30 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,7 @@
-self.addEventListener("push", function (event) {
-  let data = {};
-  try {
-    data = event.data.json();
-  } catch (e) {}
-
-  const title = data.title || "Promo Shopee";
-  const options = {
-    body: data.body || "Cek promo terbaru hari ini",
-    icon: "/assets/shopee.png",
-    badge: "/assets/shopee.png",
-    data: {
-      url: data.url || "https://shopee.co.id"
-    }
-  };
-
-  event.waitUntil(
-    self.registration.showNotification(title, options)
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("notificationclick", function (event) {
-  event.notification.close();
-  const target = event.notification.data.url;
-  event.waitUntil(
-    clients.openWindow(target)
-  );
+self.addEventListener("activate", () => {
+  self.clients.claim();
 });
